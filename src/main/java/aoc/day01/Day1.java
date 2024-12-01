3b3f7b5a51a0dab6dff62e9c69ebde9d3dc9a47f
MaciejSocha <maciek.socha1@gmail.com>
2024-12-01 09:26:26 +0100

    Init commit.

---
+++ src/main/java/aoc/day01/Day1.java
@@ -0,0 +1,9 @@
+package aoc.day01;
+
+import utils.FileReader;
+
+public class Day1 {
+    public static void main(String[] args) {
+        String input = FileReader.read("src/main/java/aoc/day01/text_input.txt");
+    }
+}
