aa82d06b620408360d4a44ace5904b7d8992a86c
Maciej Socha <Maciej.Socha@equiniti.com>
2024-12-02 10:20:23 +0100

    Part 2 of Day 2 done

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ My adventofcode.com solutions for 2024 event!
 
 [![Day01](https://img.shields.io/badge/Day1-Done-brightgreen.svg)](https://i.imgur.com/rolF402.gif)
 
-[![Day02](https://img.shields.io/badge/Day2-Waiting-lightgrey.svg)](https://i.imgur.com/FuZyZrn.gif)
+[![Day02](https://img.shields.io/badge/Day2-Done-brightgreen.svg)](https://i.imgur.com/FuZyZrn.gif)
 
 [![Day03](https://img.shields.io/badge/Day3-Waiting-lightgrey.svg)](https://i.imgur.com/FuZyZrn.gif)
 
--- src/main/java/aoc/day02/Day2.java
+++ src/main/java/aoc/day02/Day2.java
@@ -10,26 +10,52 @@ public class Day2 {
         String input = FileReader.read("src/main/java/aoc/day02/input.txt");
 
         final int[] numberOfSafeReports = {0};
-        input.lines().forEach((String line) -> {
-            numberOfSafeReports[0] += isSafe(line);
-        });
+        input.lines().forEach((String line) -> numberOfSafeReports[0] += isSafe(DataConversion.convStringLineToInt(line), 1));
 
         System.out.println(numberOfSafeReports[0]);
     }
 
-    private static int isSafe(String line) {
-        final int maxDiff = 3;
-        List<Integer> ints = DataConversion.convStringLineToInt(line);
-        final int order = getNumbersOrder(ints.get(0), ints.get(1));
+    private static int isSafe(List<Integer> ints, int maxRemovedLevel) {
+        final int order = validateOrder(ints);
+        int removedLevels = 0;
 
         for (int i = 0; i < ints.size() - 1; i++) {
             int firstNum = ints.get(i);
             int secondNum = ints.get(i + 1);
-            if (Math.abs(firstNum - secondNum) == 0 ||
-                    Math.abs(firstNum - secondNum) > maxDiff ||
-                    getNumbersOrder(firstNum, secondNum) != order) return 0;
+            if (checkIfLevelsAreNotRight(firstNum, secondNum, order)) {
+                if (removedLevels < maxRemovedLevel) {
+                    List<Integer> secondList = new java.util.ArrayList<>(ints.stream().toList());
+                    secondList.remove(i + 1);
+                    if (isSafe(secondList, 0) == 0) {
+                        List<Integer> thirdList = new java.util.ArrayList<>(ints.stream().toList());
+                        thirdList.remove(i);
+                        return isSafe(thirdList, 0);
+                    }
+                } else {
+                    return 0;
+                }
+            }
         }
         return 1;
+    }
+
+    private static boolean checkIfLevelsAreNotRight(int firstNumber, int secondNumber, int order) {
+        final int maxDifference = 3;
+        return Math.abs(firstNumber - secondNumber) == 0 ||
+                Math.abs(firstNumber - secondNumber) > maxDifference ||
+                getNumbersOrder(firstNumber, secondNumber) != order;
+    }
+
+    // 0 - decreasing, 1 - increasing
+    private static int validateOrder(List<Integer> line) {
+        int desc = 0;
+        int incr = 0;
+        for (int i = 0; i < line.size() - 1; i++) {
+            if (getNumbersOrder(line.get(i), line.get(i + 1)) == 0) desc++;
+            else incr++;
+        }
+        if (desc > incr) return 0;
+        else return 1;
     }
 
     // 0 - decreasing, 1 - increasing
