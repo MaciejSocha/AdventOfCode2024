bbc88680f3b13700e36ad5b8063c6bf26ebe58f2
Maciej Socha <Maciej.Socha@equiniti.com>
2024-12-04 12:25:32 +0100

    Part 1 of Day 4 Done

---
+++ src/main/java/aoc/day04/Day4.java
@@ -0,0 +1,60 @@
+package aoc.day04;
+
+import utils.FileReader;
+
+import java.util.List;
+
+public class Day4 {
+    private static int rowCount;
+    private static int rowLength;
+
+    public static void main(String[] args) {
+        int totalCount = 0;
+        String input = FileReader.read("src/main/java/aoc/day04/input.txt");
+        List<String> rows = input.lines().toList();
+        char[][] matrix = convertToMatrix(input);
+        MatrixSearch searches = new MatrixSearch(matrix, rowCount, rowLength);
+
+        int rowsCount = searches.searchRows(rows);
+        System.out.println("Rows: " + rowsCount);
+
+        int rowsBackwardsCount = searches.searchRowsBackwards(rows);
+        System.out.println("Rows back: " + rowsBackwardsCount);
+
+        int columnsCount = searches.searchColumns();
+        System.out.println("Columns: "+columnsCount);
+
+        int columnsBackwardCount = searches.searchColumnsBackwards();
+        System.out.println("Columns back: "+columnsBackwardCount);
+
+        List<String> diagonals = searches.searchRightDiagonals();
+        int diagonalsRightCount = searches.countWordInList(diagonals);
+        System.out.println("Diagonals right: "+diagonalsRightCount);
+        int diagonalsRightBackwardsCount = searches.countWordBackwardsInList(diagonals);
+        System.out.println("Diagonals right back: "+diagonalsRightBackwardsCount);
+
+        List<String> diagonalsLeft = searches.searchLeftDiagonals();
+        int diagonalsLeftCount = searches.countWordInList(diagonalsLeft);
+        System.out.println("Diagonals left: "+diagonalsLeftCount);
+        int diagonalsLeftBackwardsCount = searches.countWordBackwardsInList(diagonalsLeft);
+        System.out.println("Diagonals left back: "+diagonalsLeftBackwardsCount);
+
+        totalCount = rowsCount + rowsBackwardsCount + columnsCount + columnsBackwardCount + diagonalsRightCount + diagonalsRightBackwardsCount + diagonalsLeftCount + diagonalsLeftBackwardsCount;
+        System.out.println(totalCount);
+    }
+
+    private static char[][] convertToMatrix(String input) {
+        List<String> list = input.lines().toList();
+        rowCount = list.size();
+        rowLength = list.get(0).length();
+        char[][] matrix = new char[list.size()][list.get(0).length()];
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).length(); j++) {
+                matrix[i][j] = list.get(i).charAt(j);
+            }
+        }
+        return matrix;
+    }
+
+
+}
---
+++ src/main/java/aoc/day04/MatrixSearch.java
@@ -0,0 +1,118 @@
+package aoc.day04;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatrixSearch {
+    private final char[][] matrix;
+    private final int rowCount;
+    private final int rowLength;
+
+    public MatrixSearch(char[][] matrix, int rowCount, int rowLength) {
+        this.matrix = matrix;
+        this.rowCount = rowCount;
+        this.rowLength = rowLength;
+    }
+
+    public int searchRows(List<String> rows) {
+        int count = 0;
+        for (String row : rows) {
+            if (row.contains("XMAS")) {
+                count += StringUtils.countMatches(row, "XMAS");
+            }
+        }
+        return count;
+    }
+
+    public int searchRowsBackwards(List<String> rows) {
+        int count = 0;
+        for (String row : rows) {
+            if (row.contains("SAMX")) {
+                count += StringUtils.countMatches(row, "SAMX");
+            }
+        }
+        return count;
+    }
+
+    public int searchColumns() {
+        String targetWord = "XMAS";
+        int count = 0;
+        String word = "";
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < rowCount - 3; j++) {
+                word = String.valueOf(matrix[j][i]) + String.valueOf(matrix[j + 1][i]) +
+                        String.valueOf(matrix[j + 2][i] + String.valueOf(matrix[j + 3][i]));
+                if (word.equals(targetWord)) count++;
+            }
+        }
+        return count;
+    }
+
+    public int searchColumnsBackwards() {
+        String targetWord = "SAMX";
+        int count = 0;
+        String word = "";
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < rowCount - 3; j++) {
+                word = String.valueOf(matrix[j][i]) + String.valueOf(matrix[j + 1][i]) +
+                        String.valueOf(matrix[j + 2][i] + String.valueOf(matrix[j + 3][i]));
+                if (word.equals(targetWord)) count++;
+            }
+        }
+        return count;
+    }
+
+    public List<String> searchRightDiagonals() {
+        List<String> listOfDiagonals = new ArrayList<>();
+        StringBuilder diagonal = new StringBuilder();
+        for (int i = 1; i <= (rowCount + rowLength - 1); i++) {
+            int start = Math.max(0, i - rowCount);
+            int number = Math.min(Math.min(i, (rowLength - start)), rowCount);
+            for (int j = 0; j < number; j++) {
+                diagonal.append(matrix[Math.min(rowCount, i) - j - 1][start + j]);
+            }
+            listOfDiagonals.add(diagonal.toString());
+            diagonal = new StringBuilder();
+        }
+        return listOfDiagonals;
+    }
+
+    public List<String> searchLeftDiagonals() {
+        List<String> listOfDiagonals = new ArrayList<>();
+        StringBuilder diagonal = new StringBuilder();
+        for (int i = 1; i <= (rowCount + rowLength - 1); i++) {
+            int start = Math.max(0, i - rowCount);
+            int number = Math.min(Math.min(i, (rowLength - start)), rowCount);
+            for (int j = number - 1; j >= 0; j--) {
+                int first = rowCount - 1 - (Math.min(rowCount, i) - j - 1);
+                int second = start + j;
+                diagonal.append(matrix[first][second]);
+            }
+            listOfDiagonals.add(diagonal.toString());
+            diagonal = new StringBuilder();
+        }
+        return listOfDiagonals;
+    }
+
+    public int countWordInList(List<String> list) {
+        int count = 0;
+        for (String diagonal : list) {
+            if (diagonal.contains("XMAS")) {
+                count += StringUtils.countMatches(diagonal, "XMAS");
+            }
+        }
+        return count;
+    }
+
+    public int countWordBackwardsInList(List<String> list) {
+        int count = 0;
+        for (String diagonal : list) {
+            if (diagonal.contains("SAMX")) {
+                count += StringUtils.countMatches(diagonal, "SAMX");
+            }
+        }
+        return count;
+    }
+}
